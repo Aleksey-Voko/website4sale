0c019020db45e7f8e64eb56771f56527db780f83
Aleksey Voko <voko.aleksey@gmail.com>
2021-10-29 21:13:29 +0400

    style(parsing): fix style 'get catalog links'

--- bravo/parsing/catalog.py
+++ bravo/parsing/catalog.py
@@ -6,9 +6,11 @@ from bravo.utils import get_html
 
 def get_catalog_links(html_code) -> list:
     catalog_links = []
+
     soup = BeautifulSoup(html_code, 'html.parser')
     navbar_tag = soup.find('ul', {'class': 'nav navbar-nav'})
     li_tags = navbar_tag.contents
+
     for li_tag in li_tags:
         if li_tag:
             a_tag = li_tag.find('a')
@@ -17,6 +19,7 @@ def get_catalog_links(html_code) -> list:
                 if href == '#':
                     href = f'{BASE_URL}mezhkomnatnye_dveri/'
                 catalog_links.append(href)
+
     return catalog_links
 
 
