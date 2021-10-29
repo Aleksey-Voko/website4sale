cf55fa4f432c141733be48afd1f9f381130ec4f8
Aleksey Voko <voko.aleksey@gmail.com>
2021-10-29 21:11:01 +0400

    feature(parsing): add 'get page links'

--- bravo/__main__.py
+++ bravo/__main__.py
@@ -1,7 +1,8 @@
 from bravo.data import BASE_URL
-from bravo.parsing.catalog import get_catalog_links
+from bravo.parsing.catalog import get_catalog_links, get_page_links
 from bravo.utils import get_html
 
 
 catalog_html_code = get_html(BASE_URL)
 catalog_links = get_catalog_links(catalog_html_code)
+page_links = get_page_links(catalog_links)
--- bravo/parsing/catalog.py
+++ bravo/parsing/catalog.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 
 from bravo.data import BASE_URL
+from bravo.utils import get_html
 
 
 def get_catalog_links(html_code) -> list:
@@ -17,3 +18,40 @@ def get_catalog_links(html_code) -> list:
                     href = f'{BASE_URL}mezhkomnatnye_dveri/'
                 catalog_links.append(href)
     return catalog_links
+
+
+def get_page_links(catalog_links: list) -> list:
+    page_links = []
+
+    for url in catalog_links:
+        page_links.append(url)
+        page_count = get_page_count(url)
+        if page_count:
+            page_links += generate_page_links(url, page_count)
+
+    return page_links
+
+
+def get_page_count(url):
+    html_code = get_html(url)
+    soup = BeautifulSoup(html_code, 'html.parser')
+
+    pagination_tag = soup.find('ul', {'class': 'pagination'})
+
+    if pagination_tag:
+        li_tags = pagination_tag.find_all('li')
+        last_a_tag = li_tags[-1].find('a')
+        href = last_a_tag.get('href')
+        count = href.split('=')[-1]
+        return int(count)
+    else:
+        return None
+
+
+def generate_page_links(url, count):
+    page_links = []
+
+    for page in range(2, count + 1):
+        page_links.append(f'{url}?page={page}')
+
+    return page_links
