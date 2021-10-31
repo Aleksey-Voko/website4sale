85c2c6f3223a33dabd0237022369dee8fb746246
Aleksey Voko <voko.aleksey@gmail.com>
2021-11-01 03:46:52 +0400

    fix(parsing): fix 'get product cards'

--- bravo/data.py
+++ bravo/data.py
@@ -1,4 +1,6 @@
 BASE_URL = 'https://door.website4sale.online/'
+PRODUCT_PREFIX_URL = ('https://door.website4sale.online/index.php'
+                      '?route=product/product&product_id=')
 
 USER_AGENT = ('Mozilla/5.0 (iPhone; CPU iPhone OS 14_7 like Mac OS X) '
               'AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/92.0.4515.90 '
--- bravo/parsing/product_card.py
+++ bravo/parsing/product_card.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-from bravo.data import HEADERS
+from bravo.data import HEADERS, PRODUCT_PREFIX_URL
 
 
 def get_product_cards(links):
@@ -31,7 +31,12 @@ def get_product_card(link, sess: requests.sessions.Session) -> dict:
                 product_card['meta_description'] = get_meta_description(soup)
                 product_card['keywords'] = get_keywords(soup)
                 product_card['h1'] = get_h1(soup)
-                product_card['images'] = get_images(soup)
+
+                images, owl_images = get_images(soup)
+                product_card['images'] = images
+                product_card['owl_images'] = owl_images
+
+                product_card['Описание'] = get_description(soup)
                 product_card['Характеристики'] = get_specification(soup)
 
                 product_card[
@@ -99,29 +104,42 @@ def get_h1(soup: BeautifulSoup):
     return h1_tag.text.strip()
 
 
-def get_images(soup: BeautifulSoup) -> list:
+def get_images(soup: BeautifulSoup) -> tuple:
     images = []
 
     group_1_tag = soup.find('div', {'class': 'hpm-group hpm-group-1'})
-    hpm_item_tags = group_1_tag.find_all('div', {'class': 'hpm-item'})
-    for hpm_item_tag in hpm_item_tags:
-        img_tag = hpm_item_tag.find('img')
-        img = {
-            'name': img_tag.get('title'),
-            'color': img_tag.get('src'),
-            'data-id': hpm_item_tag.get('data-id'),
-        }
-        data_id = img['data-id'].split(',')
-        image = get_image(data_id[0])
-        img['image'] = image
-        images.append(img)
+    if group_1_tag:
+        hpm_item_tags = group_1_tag.find_all('div', {'class': 'hpm-item'})
+        for hpm_item_tag in hpm_item_tags:
+            img = {'data-id': hpm_item_tag.get('data-id')}
 
-    return images
+            img_tag = hpm_item_tag.find('img')
+            if img_tag:
+                img['name'] = img_tag.get('title')
+                img['color'] = img_tag.get('src')
+            else:
+                img['name'] = ''
+                img['color'] = ''
+
+            data_id = img['data-id'].split(',')
+            image = get_image(data_id[0])
+            img['image'] = image
+            images.append(img)
+
+    owl_images = []
+
+    owl_img_tag = soup.find('div', {'id': 'owl-images'})
+    if owl_img_tag:
+        item_tags = owl_img_tag.find_all('div', {'class': 'item'})
+        for item_tag in item_tags:
+            a_tag = item_tag.find('a')
+            owl_images.append(a_tag.get('id'))
+
+    return images, owl_images
 
 
 def get_image(var_id):
-    link = ('https://door.website4sale.online/'
-            f'index.php?route=product/product&product_id={var_id}')
+    link = f'{PRODUCT_PREFIX_URL}{var_id}'
     print(link)
     with requests.Session() as sess:
         response = sess.get(link, headers=HEADERS)
@@ -136,6 +154,12 @@ def get_image(var_id):
         href = a_tag.get('href')
 
         return href
+
+
+def get_description(soup: BeautifulSoup):
+    description_tag = soup.find('div', {'itemprop': 'description'})
+    description = description_tag.text.strip()
+    return description
 
 
 def get_specification(soup: BeautifulSoup) -> dict:
@@ -192,33 +216,35 @@ def get_components(soup: BeautifulSoup) -> list:
     components = []
 
     complection_tag = soup.find('div', {'id': 'tab-complection'})
-    form_group_tags = complection_tag.find_all('div', {'class': 'form-group'})
-    for form_group_tag in form_group_tags:
-        table_tag = form_group_tag.find('table')
+    if complection_tag:
+        form_group_tags = complection_tag.find_all(
+            'div', {'class': 'form-group'})
+        for form_group_tag in form_group_tags:
+            table_tag = form_group_tag.find('table')
 
-        component = {}
-        head_keys = []
-        thead_tag = table_tag.find('thead')
-        td_tags = thead_tag.find_all('td')
-        for td_tag in td_tags:
-            td_text = td_tag.text.strip()
-            if td_text:
-                if td_text != 'Количество:':
-                    head_keys.append(td_text)
-            else:
-                head_keys.append('image')
-
-        tbody_tag = table_tag.find('tbody')
-        tr_tags = tbody_tag.find_all('tr')
-        for tr_tag in tr_tags:
-            td_tags = tr_tag.find_all('td')
-            for head_key, td_tag, in zip(head_keys, td_tags):
-                img_tag = td_tag.find('img')
-                if img_tag:
-                    component[head_key] = img_tag.get('src')
+            component = {}
+            head_keys = []
+            thead_tag = table_tag.find('thead')
+            td_tags = thead_tag.find_all('td')
+            for td_tag in td_tags:
+                td_text = td_tag.text.strip()
+                if td_text:
+                    if td_text != 'Количество:':
+                        head_keys.append(td_text)
                 else:
-                    component[head_key] = td_tag.text.strip()
-            components.append(component)
+                    head_keys.append('image')
+
+            tbody_tag = table_tag.find('tbody')
+            tr_tags = tbody_tag.find_all('tr')
+            for tr_tag in tr_tags:
+                td_tags = tr_tag.find_all('td')
+                for head_key, td_tag, in zip(head_keys, td_tags):
+                    img_tag = td_tag.find('img')
+                    if img_tag:
+                        component[head_key] = img_tag.get('src')
+                    else:
+                        component[head_key] = td_tag.text.strip()
+                components.append(component)
 
     return components
 
@@ -227,23 +253,33 @@ def get_dimensions(soup: BeautifulSoup) -> list:
     dimensions = []
 
     group_0_tag = soup.find('div', {'class': 'hpm-group hpm-group-0'})
-    hpm_item_tags = group_0_tag.find_all('div', {'class': 'hpm-item'})
-    for hpm_item_tag in hpm_item_tags:
-        dim = {
-            'name': hpm_item_tag.get('data-key'),
-            'data-id': hpm_item_tag.get('data-id'),
-        }
-        data_id = dim['data-id'].split(',')
-        dim['prices'] = get_prices(data_id[0])
+    if group_0_tag:
+        hpm_item_tags = group_0_tag.find_all('div', {'class': 'hpm-item'})
+        if hpm_item_tags:
+            for hpm_item_tag in hpm_item_tags:
+                dim = {
+                    'name': hpm_item_tag.get('data-key'),
+                    'data-id': hpm_item_tag.get('data-id'),
+                }
+                data_id = dim['data-id'].split(',')
+                dim['prices'] = get_prices(data_id[0])
 
-        dimensions.append(dim)
+                dimensions.append(dim)
+        else:
+            hpm_item_tag = group_0_tag.find('div', {'class': 'hpm-item'})
+            dim = {
+                'name': hpm_item_tag.get('data-key'),
+                'data-id': hpm_item_tag.get('data-id'),
+            }
+            data_id = dim['data-id'].split(',')
+            dim['prices'] = get_prices(data_id[0])
+            dimensions.append(dim)
 
     return dimensions
 
 
 def get_prices(var_id) -> dict:
-    link = ('https://door.website4sale.online/'
-            f'index.php?route=product/product&product_id={var_id}')
+    link = f'{PRODUCT_PREFIX_URL}{var_id}'
     print(link)
     with requests.Session() as sess:
         response = sess.get(link, headers=HEADERS)
@@ -256,7 +292,10 @@ def get_prices(var_id) -> dict:
 
         prices_main_tag = soup.find('div', {'class': 'product-prices-main'})
         main_label_tag = prices_main_tag.find('span', {'class': 'price-label'})
-        main_label = main_label_tag.text.strip()
+        if main_label_tag:
+            main_label = main_label_tag.text.strip()
+        else:
+            main_label = 'main_price'
 
         main_old_price_tag = prices_main_tag.find(
             'span', {'class': 'oldprice'}
@@ -281,38 +320,39 @@ def get_prices(var_id) -> dict:
         prices_complection_tag = soup.find(
             'div', {'class': 'product-prices-complection'}
         )
-        compl_label_tag = prices_complection_tag.find(
-            'span', {'class': 'price-label'}
-        )
-        compl_label = compl_label_tag.text.strip()
-
-        compl_old_price_tag = prices_complection_tag.find(
-            'span', {'class': 'price-old'}
-        )
-        if compl_old_price_tag:
-            compl_old_price = compl_old_price_tag.text.strip()
-            compl_old_price = compl_old_price.replace(' р.', '')
-            compl_old_price = compl_old_price.replace(' ', '')
-            compl_old_price = int(compl_old_price)
-
-            compl_price_tag = prices_complection_tag.find(
-                'span', {'class': 'price-new'}
+        if prices_complection_tag:
+            compl_label_tag = prices_complection_tag.find(
+                'span', {'class': 'price-label'}
             )
-            compl_price = compl_price_tag.text.strip()
-        else:
-            compl_old_price = ''
-            compl_price_tag = prices_complection_tag.find(
-                'div', {'class': 'product-price-complection'}
+            compl_label = compl_label_tag.text.strip()
+
+            compl_old_price_tag = prices_complection_tag.find(
+                'span', {'class': 'price-old'}
             )
-            compl_price = compl_price_tag.text.strip()
+            if compl_old_price_tag:
+                compl_old_price = compl_old_price_tag.text.strip()
+                compl_old_price = compl_old_price.replace(' р.', '')
+                compl_old_price = compl_old_price.replace(' ', '')
+                compl_old_price = int(compl_old_price)
 
-        compl_price = compl_price.replace(' р.', '')
-        compl_price = compl_price.replace(' ', '')
-        compl_price = int(compl_price)
+                compl_price_tag = prices_complection_tag.find(
+                    'span', {'class': 'price-new'}
+                )
+                compl_price = compl_price_tag.text.strip()
+            else:
+                compl_old_price = ''
+                compl_price_tag = prices_complection_tag.find(
+                    'div', {'class': 'product-price-complection'}
+                )
+                compl_price = compl_price_tag.text.strip()
 
-        prices[compl_label] = {
-            'old_price': compl_old_price,
-            'price': compl_price,
-        }
+            compl_price = compl_price.replace(' р.', '')
+            compl_price = compl_price.replace(' ', '')
+            compl_price = int(compl_price)
+
+            prices[compl_label] = {
+                'old_price': compl_old_price,
+                'price': compl_price,
+            }
 
         return prices
