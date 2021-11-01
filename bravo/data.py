1551e7e3594193e652342859e0395e492322f39b
Aleksey Voko <voko.aleksey@gmail.com>
2021-11-01 13:52:32 +0400

    refactor(bravo): refactoring 'data.py'

--- bravo/data.py
+++ bravo/data.py
@@ -1,5 +1,12 @@
+from pathlib import Path
+from urllib.parse import urljoin
+
 BASE_URL = 'https://door.website4sale.online/'
-PRODUCT_PREFIX_URL = f'{BASE_URL}index.php?route=product/product&product_id='
+
+PRODUCT_PREFIX_URL = urljoin(
+    BASE_URL,
+    'index.php?route=product/product&product_id='
+)
 
 USER_AGENT = ('Mozilla/5.0 (iPhone; CPU iPhone OS 14_7 like Mac OS X) '
               'AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/92.0.4515.90 '
@@ -7,6 +14,8 @@ USER_AGENT = ('Mozilla/5.0 (iPhone; CPU iPhone OS 14_7 like Mac OS X) '
 
 HEADERS = {'User-Agent': USER_AGENT}
 
-PRODUCT_LINKS_FILE = 'out_data/product_links.yaml'
-PRODUCT_CARDS_FILE = 'out_data/product_cards.yaml'
-PRODUCT_CARDS_JSON_FILE = 'out_data/product_cards.json'
+OUT_DIR = Path('out_data')
+PRODUCT_LINKS_FILE = OUT_DIR / 'product_links.yaml'
+PRODUCT_CARDS_FILE = OUT_DIR / 'product_cards.yaml'
+PRODUCT_CARDS_JSON_FILE = OUT_DIR / 'product_cards.json'
+IMG_DIR = OUT_DIR / 'img'
